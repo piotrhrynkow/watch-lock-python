10dd5bcd658ca808834aa981f432b729515f1e97
piotrhrynkow <piotr.hrynkow@gmail.com>
2020-08-18 20:09:03 +0200

    Rename classes Yaml and Json

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from model.package import Package
 from model.package_collection import Collection
 
 
-class Yaml:
+class YamlParser:
 
     def __init__(self, path: str):
         self.path = path
@@ -39,7 +39,7 @@ class Yaml:
         return packages
 
 
-class Json:
+class JsonParser:
 
     @staticmethod
     def get_names(path: Path) -> List[str]:
@@ -88,16 +88,16 @@ class Matcher:
     @staticmethod
     def get_matches(config_path: str) -> List[Match]:
         matches = []
-        yaml = Yaml(config_path)
+        yaml = YamlParser(config_path)
         for directory in yaml.get_directories():
             yaml_packages = yaml.get_packages()
             json_paths = FileFinder.get_file_paths(directory, "json")
             for json_path in json_paths:
-                for json_package_name in Json.get_names(json_path):
+                for json_package_name in JsonParser.get_names(json_path):
                     yaml_package = yaml_packages.find_by_name(json_package_name)
                     if yaml_package is not None:
                         lock_path = FileFinder.get_lock_path(json_path)
-                        for lock_package in Json.get_packages(lock_path.absolute()):
+                        for lock_package in JsonParser.get_packages(lock_path.absolute()):
                             if lock_package.name == json_package_name:
                                 matches.append(Match(lock_path.resolve().parent, yaml_package, lock_package))
                                 break
